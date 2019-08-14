cd436e32c6c7201dadaf0018b81d232eb2c0632d
Michael Mottola <mottola64@gmail.com>
2019-08-13 22:42:40 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,13 +17,21 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
-  def update
-    @post = Post.find(params[:id])
-    @post.update(params.require(:post))
-    redirect_to post_path(@post)
-  end
 
   def edit
     @post = Post.find(params[:id])
   end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit!
+  end
+  
 end
